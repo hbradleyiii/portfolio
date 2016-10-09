80044c9a41e3979861865262bb643f71564eeae1
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-08 20:48:26 -0400

    Adds migrations for page and article tables.

---
+++ database/migrations/2016_10_09_003815_pages.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Pages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->integer('author_id')
+                  ->unsigned()
+                  ->default(0);
+            $table->foreign('author_id')
+                  ->references('id')->on('users')
+                  ->delete('cascade');
+            $table->string('title')->unique();
+            $table->text('body');
+            $table->boolean('published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
---
+++ database/migrations/2016_10_09_003819_articles.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Articles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->integer('author_id')
+                  ->unsigned()
+                  ->default(0);
+            $table->foreign('author_id')
+                  ->references('id')->on('users')
+                  ->delete('cascade');
+            $table->string('title')->unique();
+            $table->text('body');
+            $table->boolean('published');
+            $table->date('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
