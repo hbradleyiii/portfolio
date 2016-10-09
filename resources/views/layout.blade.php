b0486e4a45e0c86f6a40b7c3de7266060f60e0c3
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-09 15:21:16 -0400

    Adds basic default blade layout template.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="author" content="Harold Bradley III">
+    <meta name="copyright" content="&copy; {{ date('Y') }} Harold Bradley III">
+    <meta name="description" content="@yield('meta_description')">
+
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    @include('header')
+
+    @yield('content')
+
+    @include('footer')
+
+</body>
+</html>
