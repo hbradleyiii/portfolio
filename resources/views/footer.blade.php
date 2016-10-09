36cdf2cf4b8bc3191c1dfce8dfb2e2c717156a83
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-09 14:52:29 -0400

    Adds header and footer blade template files.

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,26 @@
+    <footer>
+        <section class="work">
+            <h2>Work</h2>
+            @include('navigation.work')
+        </section>
+        <section class="skills">
+            <h2>Skills</h2>
+            @include('navigation.skills')
+        </section>
+        <section class="social">
+            <h2>Social</h2>
+            @include('navigation.social')
+        </section>
+        <section class="vita">
+            <h2>Vita</h2>
+            @include('navigation.vita')
+        </section>
+        <nav class="footer-nav">
+            @include('navigation.main')
+        </nav>
+        <span class="copyright">
+            Copyright &copy; {{ date('Y') }} -
+            {{ name_with_email() }}
+            - All rights reserved.
+        </span>
+    </footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,8 @@
+    <header>
+        <a href="/" class="signature">haroldbradleyiii.com</a>
+        <input type="checkbox" name="menu-toggle" id="menu-toggle">
+        <label for="menu-toggle">Menu</label>
+        <nav class="main-nav">
+            @include('navigation.main')
+        </nav>
+    </header>
