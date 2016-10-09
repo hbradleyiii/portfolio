1cd57db9c0fe30f1c1ce9baaf3203d73c22996c7
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-09 18:00:25 -0400

    Cleans up the admin template.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,27 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta name="author" content="Harold Bradley III">
-    <meta name="copyright" content="&copy; {{ date('Y') }} Harold Bradley III">
-    <meta name="description" content="@yield('meta_description')">
 
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-    <title>@yield('title')</title>
+    <title>HB3 | Admin</title>
 </head>
 <body>
 
-    @include('header')
-
-    @yield('content')
-
-    @include('footer')
+    <div class="container">
+        @yield('content')
+    </div>
 
 </body>
 </html>
