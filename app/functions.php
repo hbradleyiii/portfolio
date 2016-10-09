0192d8da07df5a876cc5f00b86ea5aaa7e7e5700
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-09 14:40:06 -0400

    Adds functions file.

---
+++ app/functions.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Uses http://hivelogic.com/enkoder
+ *
+ * @return string - returns an encoded email address (enocoded to prevent spam)
+ */
+function email_address() { ?>
+
+            <script type="text/javascript">
+                //<![CDATA[
+                <!--
+                    var x="function f(x,y){var i,o=\"\",l=x.length;for(i=0;i<l;i++){if(i<70)y++;" +
+                    "y%=127;o+=String.fromCharCode(x.charCodeAt(i)^(y++));}return o;}f(\"!<%.;8<" +
+                    ";w?s%v\\032\\025\\004\\025I\\002A\\000LQW[\\026\\027@x,hcfmxf<~)yt!kvINA\\0" +
+                    "16P\\004OFQ@wY\\\\_}Jh.kw{cmsdjz/!$.#!qf$f2t]\\rY\\036\\007\\004\\022\\004\\"+
+                    "000A\\017B\\027\\020\\023\\t\\037\\003Z\\032I\\031[FC\\020DFLF\\027-,+xk.;\\"+
+                    "177&jbj~Lz'y8)nfpbbjw:t2nk}SUP\\013\\024\\tIK\\001\\022WM\\004\\017\\007\\0" +
+                    "34\\006\\035n\\021XSXJIU\\n\\014\\013\\014bc3\\035`\\037\\030aka\\177xz\\02" +
+                    "7\\020:$|ca\\016\\017~ysy\\177:nhl\\001\\002mPQ>?)SWW45_X\\\\12XBA./EFF+$  " +
+                    "PONN\\\\]004YZ0:9VWQ;=?LM\\\" $IJ&*)FG.-.C|U~\\177\\t\\020\\026\\027tu\\030" +
+                    "\\033\\034qr\\034\\000\\001no\\004\\007\\006kdKfg\\017\\014\\016c\\034srs\\" +
+                    "030\\031rwx\\025\\026}~}\\022\\023aab\\017\\010;\\n\\013\\003nkk\\000\\001u" +
+                    "mT\\\\T\\033QUV;4\\014[XKQ\\035\\\\\\035\\003\\025SU\\\\ITCF\\023IW\\\\P7] " +
+                    "_X*][*!lv7b.abdfvfh->121b4C7v\\010UcF@JeUIAI\\005T\\005KK_r@R\\\\v[XJ_\\024" +
+                    "\\\\RTLK\\023|i,\\177rtvul3p%pe6ybdni<~11\\\"spw4e3\\\\\\010YSY\\014N\\025\\"+
+                    "007\\017Q\\003\\030\\n\\000\\n\\034_\\nN\\030Y*U$[G\\024P\\024^r`tx-|*\\177" +
+                    " o*ecdzl~dt1=\",70)"                                                         ;
+                    while(x=eval(x));
+                //-->
+                //]]>
+            </script>
+
+<?php }
