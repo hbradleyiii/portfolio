50d03fd4f7d759438f693e184b718468b43a56dd
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-09 21:39:56 -0400

    Adds comments.

--- app/Page.php
+++ app/Page.php
@@ -14,6 +14,11 @@ class Page extends Model {
 
     protected $fillable = ['title', 'slug', 'body', 'published'];
 
+    /**
+     * Use slug as RouteKeyName.
+     *
+     * @return string - database column
+     */
     public function getRouteKeyName() {
         return 'slug';
     }
