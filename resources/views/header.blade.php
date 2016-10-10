ba0afd5aeef81816cf91fc3015a4ba93eb707fe6
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-10 06:48:46 -0400

    Makes menu button hidden when css is broken.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,7 @@
     <header>
         <a href="/" class="signature">haroldbradleyiii.com</a>
-        <input type="checkbox" name="menu-toggle" id="menu-toggle">
-        <label for="menu-toggle">Menu</label>
+        <input type="checkbox" name="menu-toggle" id="menu-toggle" style="display: none;">
+        <label for="menu-toggle"></label>
         <nav class="main-nav">
             @include('navigation.main')
         </nav>
