430523c60cfaf4a27f16af31dfa7c3ea95edb695
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-10 06:50:27 -0400

    Removes a spurious space.

--- resources/views/navigation/work.blade.php
+++ resources/views/navigation/work.blade.php
@@ -1,3 +1,3 @@
-        @foreach ( $featured_projects as $project)
+        @foreach ($featured_projects as $project)
             <a href="{{ URL::route('project.show', $project->slug) }}">{{ $project->name }}</a>
         @endforeach
