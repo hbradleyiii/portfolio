e9411a86022b0a64087e7d8b8d93637652987885
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-09 21:34:45 -0400

    Changes array_merge 2 vars to 1.

--- resources/views/navigation/work.blade.php
+++ resources/views/navigation/work.blade.php
@@ -1,3 +1,3 @@
-        @foreach ( array_merge($featured_websites, $featured_projects) as $project)
+        @foreach ( $featured_projects as $project)
             <a href="{{ URL::route('project.show', $project->slug) }}">{{ $project->name }}</a>
         @endforeach
