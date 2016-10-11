814b40269e52fa00c775b5cc36676ad53ab30bf3
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-10 21:05:45 -0400

    Corrects a misnamed route.

--- resources/views/navigation/work.blade.php
+++ resources/views/navigation/work.blade.php
@@ -1,3 +1,3 @@
         @foreach ($featured_projects as $project)
-            <a href="{{ URL::route('project.show', $project->slug) }}">{{ $project->name }}</a>
+            <a href="{{ URL::route('projects.show', $project->slug) }}">{{ $project->name }}</a>
         @endforeach
