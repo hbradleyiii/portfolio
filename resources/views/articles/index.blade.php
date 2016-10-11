cd1b49ec22126d1beba113ffa6f318cf6ec9fbee
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-10 20:38:25 -0400

    Updates article views.

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,26 +7,10 @@
             <ul class="list-group">
                 @foreach ($articles as $article)
                     <li class="list-group-item">
-                        <div>
-                            <a href="{{ URL::route('articles.edit', $article->slug) }}">{{ $article->title }}</a>
-                        </div>
-                        <div class="btn-group">
-                            <a href="{{ URL::route('articles.show', $article->slug) }}" class="btn btn-sm btn-primary">View</a>
-                            <a href="{{ URL::route('articles.edit', $article->slug) }}" class="btn btn-sm btn-primary">Edit</a>
-                        </div>
-                        {{ Form::open(['method' => 'DELETE', 'route' => ['articles.destroy', $article->slug]]) }}
-                            {{ Form::submit('Delete', ['class' => 'btn btn-sm btn-danger']) }}
-                        {{ Form::close() }}
+                        <a href="{{ URL::route('articles.show', $article->slug) }}">{{ $article->title }}</a>
                     </li>
                 @endforeach
             </ul>
-        </div>
-        <hr>
-        <div class="col-md-6 col-md-offset-3">
-            <h1>Create a new article</h1>
-            {!! Form::open([ 'method' => 'POST', 'route' => ['articles.store'] ]) !!}
-                @include('articles.form')
-            {!! Form::close() !!}
         </div>
     </div>
 @stop
